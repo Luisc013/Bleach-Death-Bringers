793934b0024470fea8a529cc70382de8bb1eddca
Luisc013 <c.luis013@gmail.com>
2021-11-04 11:01:04 -0400

    Added favicon, changed delays on title screen opacity, changed html file name, added style and script sheets instead of embedding them for scalability.

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-var delayInMilliseconds = 2000; //1 second
+var delayInMilliseconds = 2000;
+var delayInMilliseconds2 = 9000; //1 second
 
 setTimeout(function() {
     startShowing();
     }, delayInMilliseconds);
+
+    setTimeout(function() {
+        startKeyShowing();
+        }, delayInMilliseconds2);
+
+    function startKeyShowing(){
+        var idToHide = document.getElementById("start-game");
+        idToHide.style.opacity = 0;
+        var intervalId = setInterval(function(){
+            if(idToHide.style.opacity >= 1)
+            {
+                clearInterval(intervalId);
+            } else{
+                idToHide.style.opacity = parseFloat(idToHide.style.opacity) + 0.05;
+            }
+        },250);
+        }
 
 function startShowing(){
     var elementToHide = document.getElementById("title-icon");
@@ -16,3 +34,4 @@ function startShowing(){
         }
     },250);
     }
+
